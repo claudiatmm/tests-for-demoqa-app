5d75f50f1195f72ddd376853d822c98741019d20
Claudia Timiș <claudia.timis95@gmail.com>
2023-10-07 11:34:20 +0300

    datePicker method

--- src/test/java/com/testingT/pages/DatePickerPage.java
+++ src/test/java/com/testingT/pages/DatePickerPage.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.springframework.stereotype.Component;
 
+import java.time.Month;
+import java.time.YearMonth;
+import java.util.Locale;
+
 
 @Component
 public class DatePickerPage extends AbstractPage {
@@ -30,9 +34,6 @@ public class DatePickerPage extends AbstractPage {
     @FindBy(how = How.CLASS_NAME, using = "ui-datepicker-title")
     private WebElement datepickerTitle;
 
-//    @FindBy(how = How.CLASS_NAME, using = "ui-datepicker-next ui-corner-all")
-//    private WebElement nextButton;
-
 
     public DatePickerPage clickOnButtonDatePicker() throws InterruptedException {
         datepicker1.click();
@@ -41,25 +42,52 @@ public class DatePickerPage extends AbstractPage {
     }
 
 
-    public DatePickerPage selectDateAndYear(String monthInput, int yearInput) throws InterruptedException {
-        String currentMonth = month.getText();
+    public DatePickerPage selectDateAndYear(String monthInputAtString, int yearInput) throws InterruptedException {
+        String currentMonthAtString = month.getText();
         int currentYear = Integer.parseInt(year.getText());
 
         Thread.sleep(1000);
-        while (!(currentMonth.equals(monthInput) && currentYear==yearInput)){
-            if (currentYear >= yearInput){
-                prevButton.click();
-                currentMonth = month.getText();
-                currentYear = Integer.parseInt(year.getText());
+
+        while (!(currentMonthAtString.equals(monthInputAtString) && currentYear==yearInput)){
+            if(yearInput > currentYear ){
+                nextButton.click();
             }
-            else if (currentYear <= yearInput){
+            if (yearInput < currentYear){
+                prevButton.click();
+            }
+
+            if (yearInput== currentYear){
+                Month targetMonth = Month.valueOf(monthInputAtString.toUpperCase(Locale.ROOT));
+                Month currentMonth = Month.valueOf(currentMonthAtString.toUpperCase(Locale.ROOT));
+               int r = targetMonth.compareTo(currentMonth);
+                if (r > 0){
                     nextButton.click();
-                    currentMonth = month.getText();
-                    currentYear = Integer.parseInt(year.getText());
+                }
+
+                if (r <0){
+                    prevButton.click();
+                }
             }
             Thread.sleep(1000);
+
+            currentMonthAtString = month.getText();
+            currentYear = Integer.parseInt(year.getText());
         }
-        Assertions.assertEquals(datepickerTitle.getText(), (monthInput +" " +yearInput));
+
+//        while (!(currentMonthAtString.equals(monthInputAtString) && currentYear==yearInput)){
+//            if (currentYear >= yearInput){
+//                prevButton.click();
+//                currentMonthAtString = month.getText();
+//                currentYear = Integer.parseInt(year.getText());
+//            }
+//            else if (currentYear <= yearInput){
+//                    nextButton.click();
+//                    currentMonthAtString = month.getText();
+//                    currentYear = Integer.parseInt(year.getText());
+//            }
+//            Thread.sleep(1000);
+//        }
+        Assertions.assertEquals(datepickerTitle.getText(), (monthInputAtString +" " +yearInput));
         return this;
     }
 
@@ -70,7 +98,6 @@ public class DatePickerPage extends AbstractPage {
             xPathWithVariable = "//td[@data-handler='selectDay']/a[text()="+"'"+i +"'"+ "]";
         }
         driver.findElement(By.xpath(xPathWithVariable)).click();
-//        driver.findElement(By.xpath("//td[@data-handler='selectDay']/a[text()='23']")).click();
         Assertions.assertEquals(day, Integer.valueOf(driver.findElement(By.xpath(xPathWithVariable)).getText()));
         return  this;
     }
